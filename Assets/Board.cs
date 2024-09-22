2be04c1d1e896033502a97a7f65151a24b40cccd
FSAGuy <127125790+FSAguy@users.noreply.github.com>
2024-09-23 02:30:13 +0300

    Test test

---
+++ Assets/Board.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private int _turnIdx;
+    [SerializeField] private List<Player> players;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
