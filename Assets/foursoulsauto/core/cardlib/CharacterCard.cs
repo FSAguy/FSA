3a58938a17019c1011d3c8fc348ce6e7c36e32f8
FSAGuy <127125790+FSAguy@users.noreply.github.com>
2024-10-22 21:37:58 +0300

    added d6

---
+++ Assets/foursoulsauto/core/cardlib/CharacterCard.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace foursoulsauto.core.cardlib
+{
+    public abstract class CharacterCard : LivingCard
+    {
+        [SerializeField] protected Card StartingItem;
+        
+        // TODO: method to grab the starting item? maybe should be implemented at Player instead?
+    }
+}
